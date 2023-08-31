0f38dffab45dcdd334eef387094677b873dc872c
Davina Leong <leong.shi.yun@gmail.com>
2023-08-31 11:48:50 +0800

    WIP modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -107,6 +107,36 @@ export default function Index() {
           year={Config.year}
         />
       </div>
+
+      <div className="modal">
+        <div className="modal__dialog | position-relative">
+          <button className="btn btn-slide position-absolute positioning-right positioning-top shadow-v-br-400">
+            Close
+          </button>
+
+          <header className="border-v-b-solid-50-gray-50 p-v-y-400">
+            <h2 className="modal__header__heading | fz-500">Lorem Ipsum</h2>
+          </header>
+
+          <div className="flow p-v-t-400">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat
+              inventore magni ipsa veritatis dolorem totam vero nulla veniam
+              laboriosam? Rerum, sunt incidunt ipsum suscipit, itaque,
+              repudiandae porro excepturi assumenda vitae aperiam eligendi
+              aliquid quia nemo. Optio eaque cupiditate voluptatem rem beatae.
+              Corrupti quasi nemo minus eos omnis architecto aliquid quaerat.
+            </p>
+
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga
+              dicta magni, vero provident animi quam. Neque temporibus facere
+              modi. Corrupti ipsa nihil porro deserunt quae cupiditate ducimus!
+              Dolor, veritatis maiores.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
