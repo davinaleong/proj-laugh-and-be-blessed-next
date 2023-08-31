d52011cd03039e8f23b6da741f1a75a6d5454885
Davina Leong <leong.shi.yun@gmail.com>
2023-08-31 10:49:08 +0800

    Copied static content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,112 @@ import styles from "./page.module.css"
 
 export default function Index() {
   return (
-    <main className="wrapper flow">
-      <h1>Test</h1>
-    </main>
+    <div className="wrapper wrapper-aligned-center | flow text-gray-50 bg-full position-relative p-v-y-400">
+      <header className="flow | border-v-b-solid-50-gray-50">
+        <h1 className="fz-700">Laugh and be Blessed</h1>
+        <p className="m-v-b-400">
+          Davina Leong's catalogue of jokes, riddles and tongue twisters.
+        </p>
+      </header>
+
+      <main className="flow">
+        <section>
+          <p>Click on one of the cards below to view the content.</p>
+        </section>
+
+        <section>
+          <div className="card-grid">
+            <a href="#" className="card bg-full shadow-v-br-400">
+              <h3 className="fz-500">Lorem ipsum dolor</h3>
+            </a>
+
+            <a href="#" className="card bg-full shadow-v-br-400">
+              <h3 className="fz-500">Lorem ipsum dolor</h3>
+            </a>
+
+            <a href="#" className="card bg-full shadow-v-br-400">
+              <h3 className="fz-500">Lorem ipsum dolor</h3>
+            </a>
+
+            <a href="#" className="card bg-full shadow-v-br-400">
+              <h3 className="fz-500">Lorem ipsum dolor</h3>
+            </a>
+
+            <a href="#" className="card bg-full shadow-v-br-400">
+              <h3 className="fz-500">Lorem ipsum dolor</h3>
+            </a>
+
+            <a href="#" className="card bg-full shadow-v-br-400">
+              <h3 className="fz-500">Lorem ipsum dolor</h3>
+            </a>
+
+            <a href="#" className="card bg-full shadow-v-br-400">
+              <h3 className="fz-500">Lorem ipsum dolor</h3>
+            </a>
+
+            <a href="#" className="card bg-full shadow-v-br-400">
+              <h3 className="fz-500">Lorem ipsum dolor</h3>
+            </a>
+
+            <a href="#" className="card bg-full shadow-v-br-400">
+              <h3 className="fz-500">Lorem ipsum dolor</h3>
+            </a>
+
+            <a href="#" className="card bg-full shadow-v-br-400">
+              <h3 className="fz-500">Lorem ipsum dolor</h3>
+            </a>
+
+            <a href="#" className="card bg-full shadow-v-br-400">
+              <h3 className="fz-500">Lorem ipsum dolor</h3>
+            </a>
+
+            <a href="#" className="card bg-full shadow-v-br-400">
+              <h3 className="fz-500">Lorem ipsum dolor</h3>
+            </a>
+
+            <a href="#" className="card bg-full shadow-v-br-400">
+              <h3 className="fz-500">Lorem ipsum dolor</h3>
+            </a>
+
+            <a href="#" className="card bg-full shadow-v-br-400">
+              <h3 className="fz-500">Lorem ipsum dolor</h3>
+            </a>
+
+            <a href="#" className="card bg-full shadow-v-br-400">
+              <h3 className="fz-500">Lorem ipsum dolor</h3>
+            </a>
+
+            <a href="#" className="card bg-full shadow-v-br-400">
+              <h3 className="fz-500">Lorem ipsum dolor</h3>
+            </a>
+
+            <a href="#" className="card bg-full shadow-v-br-400">
+              <h3 className="fz-500">Lorem ipsum dolor</h3>
+            </a>
+
+            <a href="#" className="card bg-full shadow-v-br-400">
+              <h3 className="fz-500">Lorem ipsum dolor</h3>
+            </a>
+
+            <a href="#" className="card bg-full shadow-v-br-400">
+              <h3 className="fz-500">Lorem ipsum dolor</h3>
+            </a>
+
+            <a href="#" className="card bg-full shadow-v-br-400">
+              <h3 className="fz-500">Lorem ipsum dolor</h3>
+            </a>
+          </div>
+        </section>
+      </main>
+
+      <footer>
+        <p className="ta-center fz-300">
+          <em>
+            Laugh and be Blessed &copy; Davina Leong,
+            <span data-element="copyright-year">2023</span>
+          </em>
+        </p>
+      </footer>
+    </div>
   )
 }
