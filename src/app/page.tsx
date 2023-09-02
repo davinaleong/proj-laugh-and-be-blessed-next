7de895261132c055b11bcec591f0ece1a09201fd
Davina Leong <leong.shi.yun@gmail.com>
2023-09-02 11:42:46 +0800

    Able to pull data from Contentful

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,32 +7,24 @@ import { CardComponent } from "./components/card"
 import { FooterComponent } from "./components/footer"
 import { HeaderComponent } from "./components/header"
 import { HelpComponent } from "./components/help"
-import { useState } from "react"
 
-export interface IndexPageProps {
-  jokes: any
-}
-
-export async function getStaticProps() {
+export async function getData() {
   const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    space: `${process.env.CONTENTFUL_SPACE_ID}`,
+    environment: `${process.env.CONTENTFUL_ENVIRONMENT_ID}`,
+    accessToken: `${process.env.CONTENTFUL_ACCESS_TOKEN}`,
   })
 
-  const res = await client.getEntries({
-    content_type: process.env.CONTENT_TYPE,
+  const response = await client.getEntries({
+    content_type: `${process.env.CONTENT_TYPE_ID}`,
   })
 
-  return {
-    props: {
-      jokes: "Hello",
-    },
-  }
+  return response.items
 }
 
-export default function Index(props: IndexPageProps) {
-  console.log(`props`, props)
-  // console.log(`jokes`, jokes)
+export default async function Index() {
+  const jokes = await getData()
+  console.log(`jokes`, jokes)
 
   return (
     <div className="wrapper wrapper-aligned-center | flow">
