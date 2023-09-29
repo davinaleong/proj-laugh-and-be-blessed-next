72f07802d33004ce8d75ecf5086679e907e0a464
Davina Leong <leong.shi.yun@gmail.com>
2023-09-29 15:36:43 +0800

    Added new env var

--- src/app/lib/contentful/interfaces.tsx
+++ src/app/lib/contentful/interfaces.tsx
@@ -1,6 +1,7 @@
 import { ContentfulClientApi } from "contentful"
 
 export interface LibContentfulEnvVarsInterface {
+  CONTENTFUL_API_URL: string
   CONTENTFUL_ACCESS_TOKEN: string
   CONTENTFUL_SPACE_ID: string
   CONTENTFUL_ENVIRONMENT_ID: string
--- src/app/lib/contentful/variables.tsx
+++ src/app/lib/contentful/variables.tsx
@@ -1,11 +1,13 @@
 import { LibContentfulEnvVarsInterface } from "./interfaces"
 
+export const CONTENTFUL_API_URL: string = `${process.env.CONTENTFUL_API_URL}`
 export const CONTENTFUL_SPACE_ID: string = `${process.env.CONTENTFUL_SPACE_ID}`
 export const CONTENTFUL_ENVIRONMENT_ID: string = `${process.env.CONTENTFUL_ENVIRONMENT_ID}`
 export const CONTENTFUL_ACCESS_TOKEN: string = `${process.env.CONTENTFUL_ACCESS_TOKEN}`
 export const CONTENT_TYPE_ID: string = `${process.env.CONTENT_TYPE_ID}`
 
 const LibContentfulEnvVars: LibContentfulEnvVarsInterface = {
+  CONTENTFUL_API_URL,
   CONTENTFUL_SPACE_ID,
   CONTENTFUL_ENVIRONMENT_ID,
   CONTENTFUL_ACCESS_TOKEN,
